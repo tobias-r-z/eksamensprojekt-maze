5f20f09a7b3dc92bc35a5c24d069582e471c0e42
tobias-r-z <tobiasrisager5@gmail.com>
2026-03-20 09:29:44 +0100

    vejbegyndelse

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #improtere pakker til python
 import pygame
-import random
 from pygame.locals import *
+import random as rand
 pygame.init()
 #generer skærmen ti
 screen=pygame.display.set_mode((300, 300))
@@ -10,55 +10,72 @@ screen.fill(white)
 pygame.display.update()
 #celle til labyrinten
 class Cell:
-    def __init__(self,x,y):
-        self.x=x+1*150
-        self.y=y+1*150
+    def __init__(self,x,y,scale):
+        self.x=x
+        self.y=y
+        self.scale=scale
         #skal repræsentere hvor de forskellige vægge kan være.
         self.walls = [True,True,True,True] #venstre, højre, over, nedre
-        self.visited = False
     #en metode som kan bygge væggen op omkring 
+        self.start = False
+        self.end = False
     def væggebyg(self):
         #ideen er at se om en af væggene er sande, og så tegne siderne
+        if self.walls[0] == True:
+            pygame.draw.line(screen, [0,0,0], [self.x*100,self.y*100],[self.x*100,(self.y*100)+100])
         if self.walls[1] == True:
-            pygame.draw.line(screen, [0,0,0], [self.x+50,self.y+50],[self.x+50,self.y-50])
-    def tegn(self):
-        pygame.draw.rect(screen,[0,0,200],[100, 100, 400, 100],2)        
+            pygame.draw.line(screen, [0,0,0], [(self.x*100)+100,self.y*100],[(self.x*100)+100,(self.y*100)+100])
+        if self.walls[2] == True:
+            pygame.draw.line(screen, [0,0,0], [(self.x*100)+100,self.y*100],[(self.x*100)+100,self.y*100])
+        if self.walls[3] == True:
+            pygame.draw.line(screen, [0,0,0], [self.x*100,(self.y*100)+100],[(self.x*100)+100,(self.y*100)+100])
+    
+    def givekoords(self):
+        return [self.x*100,self.y*100]
+    def startslut(self,lab):
+        if self.x==0 and self.y==0:
+            self.start = True
+        if self.x == len(lab)-1 and self.y == len(lab)-1:
+            self.end = True
 
-
-#definerer hver celle
-cell00 = Cell(0,0)
-cell01 = Cell(1,0)
-cell02 = Cell(2,0)
-cell10 = Cell(0,1)
-cell11 = Cell(1,1)
-cell12 = Cell(2,1)
-cell20 = Cell(0,2)
-cell21 = Cell(1,2)
-cell22 = Cell(2,2)
 
 #laver labyrinten som et 2-dimensionelt array
-labyrint = [[cell00, cell01, cell02],
-            [cell10, cell11, cell12],
-            [cell20, cell21, cell22]]
-
-labyrint2 = [[1, 2, 3], 
-             [1, 2, 3], 
-             [1, 2, 3]]
+labyrint2 =[[0, 1, 2], 
+            [0, 1, 2], 
+            [0, 1, 2]]
+labvej =   [[0,0,0],
+            [0,0,0],
+            [0,0,0]]
 
 def labbyg(lab):
-    for row in lab:
-        for num in row:
-            cell = Cell(num,lab.index(row))
+    for y,row in enumerate(lab):
+        for x,num in enumerate(row):
+            cell = Cell(x,y,100)
+            vej(cell)
             cell.væggebyg()
+            cell.startslut(lab)
+            #print(cell.start + cell.end)
 
-def labyrintalgo():
-    for row in labyrint:
-        for x in row:
-            #print(labyrint.index(row),row.index(x))
-            x.væggebyg()
-            x.tegn()
 
-running = True
+            
+def vej(acell):
+    if acell.start == True:
+        labvej[acell.y][acell.x] = 1
+        rnd=rand.randint(0,1)
+        if rnd == 0:
+            acell.walls[1]=False
+        if rnd == 1:
+            acell.walls[3]=False
+
+        
+    if labvej[(acell.y)-1][(acell).x]==1 or labvej[(acell).y][(acell).x-1]==1:
+        labvej[acell.y][acell.x] = 1
+    #if acell.end == True:
+       #labvej[acell.y][acell.x] = 1
+
+
+
+running = True        
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -68,12 +85,8 @@ while running:
 
     screen.fill([255,255,255])
 
-    labyrintalgo()
-
-    #pygame.draw.line(screen, [0,0,0], [75,75],[100,100])
+    labbyg(labyrint2)
 
     pygame.display.update()
-
-
 
 pygame.quit()
