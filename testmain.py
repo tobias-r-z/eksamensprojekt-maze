43d9c47ee1a3334654f2c6e1b6195da97e251a3a
tobias-r-z <tobiasrisager5@gmail.com>
2026-05-10 11:33:50 +0200

    final

--- testmain.py
+++ testmain.py
@@ -1,9 +1,7 @@
 #improtere pakker til python
 import pygame
-from pygame.locals import *
+#from pygame.locals import *
 import random as rand
-
-
 
 #celle til labyrinten
 class Cell:
@@ -17,7 +15,7 @@ class Cell:
 
     #en metode som kan bygge væggen op omkring 
     def væggebyg(self):
-        #ideen er at se om en af væggene er sande, og så tegne siderne
+        #ideen  er at se om en af væggene er sande, og så tegne siderne
         if self.walls[0] == True:
             pygame.draw.line(screen, [0,0,0], [self.x*self.scale,self.y*self.scale],[self.x*self.scale,(self.y*self.scale)+self.scale])
         if self.walls[1] == True:
@@ -26,14 +24,6 @@ class Cell:
             pygame.draw.line(screen, [0,0,0], [self.x*self.scale, self.y*self.scale], [(self.x*self.scale)+self.scale, self.y*self.scale])
         if self.walls[3] == True:
             pygame.draw.line(screen, [0,0,0], [self.x*self.scale,(self.y*self.scale)+self.scale],[(self.x*self.scale)+self.scale,(self.y*self.scale)+self.scale])
-    
-    def givekoords(self):
-        return [self.x*100,self.y*100]
-    def startslut(self,lab):
-        if self.x==0 and self.y==0:
-            self.start = True
-        if self.x == len(lab)-1 and self.y == len(lab)-1:
-            self.end = True
     
     def posvia(self,visited):
         #rows, cols = 24, 24
@@ -53,41 +43,37 @@ class Cell:
                 rnddir.append([[self.x,self.y],[nx,ny],dr])
         return rnddir
 
+
+#labyrint klasse for mere kompakt kode
 class labyrint : 
     def __init__(self):
-        self.row = rows
-        self.col = cols
-        self.grid = [[Cell(x, y, 25) for x in range(cols)] for y in range(rows)]
-        
+        self.grid = [[Cell(x, y, celles) for x in range(cols)] for y in range(rows)]
+        self.visited = [[False]*cols for _ in range(rows)]
     
-
-def remove_walls(x1 ,y1 ,x2 ,y2 , dir:str, grid):
-    if dir == "left":
-        grid[y1][x1].walls[0] = False
-        grid[y2][x2].walls[1] = False
-    elif dir == "right":
-        grid[y1][x1].walls[1] = False
-        grid[y2][x2].walls[0] = False
-    elif dir == "up":
-        grid[y1][x1].walls[2] = False
-        grid[y2][x2].walls[3] = False
-    elif dir == "down":
-        grid[y1][x1].walls[3] = False
-        grid[y2][x2].walls[2] = False
-
-
-
+    def remove_walls(self,x1 ,y1 ,x2 ,y2 , dir:str):
+        grid = self.grid
+        if dir == "left":
+            grid[y1][x1].walls[0] = False
+            grid[y2][x2].walls[1] = False
+        elif dir == "right":
+            grid[y1][x1].walls[1] = False
+            grid[y2][x2].walls[0] = False
+        elif dir == "up":
+            grid[y1][x1].walls[2] = False
+            grid[y2][x2].walls[3] = False
+        elif dir == "down":
+            grid[y1][x1].walls[3] = False
+            grid[y2][x2].walls[2] = False
 
 
 #laver labyrinten som et 2-dimensionelt array
 def generate_rb():
     #rows, cols = 24, 24
-    grid = [[Cell(x, y, 25) for x in range(cols)] for y in range(rows)]
-
-    visited = [[False]*cols for _ in range(rows)]
+    lab = labyrint()
 
     def dfs(r, c):
-        visited[r][c] = True
+
+        lab.visited[r][c] = True
 
         directions = [
             ("left", 0, -1),
@@ -101,48 +87,35 @@ def generate_rb():
         for dir, dr, dc in directions:
             nr, nc = r + dr, c + dc
 
-            if 0 <= nr < rows and 0 <= nc < cols and not visited[nr][nc]:
+            if 0 <= nr < rows and 0 <= nc < cols and not lab.visited[nr][nc]:
 
                 # fjern vægge mellem celler
-                if dir == "left":
-                    grid[r][c].walls[0] = False
-                    grid[nr][nc].walls[1] = False
-                elif dir == "right":
-                    grid[r][c].walls[1] = False
-                    grid[nr][nc].walls[0] = False
-                elif dir == "up":
-                    grid[r][c].walls[2] = False
-                    grid[nr][nc].walls[3] = False
-                elif dir == "down":
-                    grid[r][c].walls[3] = False
-                    grid[nr][nc].walls[2] = False
-
+                lab.remove_walls(c,r,nc,nr,dir)
+                
                 dfs(nr, nc)
 
     dfs(0, 0)
-    return grid
+    return lab.grid
 
 def generate_prim():
     #rows, cols = 24, 24
-    grid = [[Cell(x, y, 25) for x in range(cols)] for y in range(rows)]
-    visited = [[False]*cols for _ in range(rows)]
-
+    lab = labyrint()
     frontier = []
 
-    visited[0][0] = True
-    frontier.extend(grid[0][0].posvia(visited))
+    lab.visited[0][0] = True
+    frontier.extend(lab.grid[0][0].posvia(lab.visited))
 
     while frontier:
         rand.shuffle(frontier)
         (x1,y1),(x2,y2),direction = frontier.pop()
 
-        if not visited[y2][x2]:
-            remove_walls(x1,y1,x2,y2,direction,grid)
+        if not lab.visited[y2][x2]:
+            lab.remove_walls(x1,y1,x2,y2,direction)
 
-            visited[y2][x2] = True
-            frontier.extend(grid[y2][x2].posvia(visited))
+            lab.visited[y2][x2] = True
+            frontier.extend(lab.grid[y2][x2].posvia(lab.visited))
 
-    return grid
+    return lab.grid
 
 def labwhole(grid):
     
@@ -155,12 +128,12 @@ def labwhole(grid):
 algo_input = input("Hvilken algoritme skal bruges? ")
 cols = int(input("Hvor bred skal labyrinten være? "))
 rows = int(input("Hvor høj skal labyrinten være? "))
-celler = int(input("hvor store skal cellerne være (kvadrat)? "))
+celles = int(input("hvor store skal cellerne være (kvadrat)? "))
 
 pygame.init()
 
 #generer skærmen ti
-screen=pygame.display.set_mode((cols*25, rows*25))
+screen=pygame.display.set_mode((cols*celles, rows*celles))
 
 white = [255, 255, 255]
 
@@ -168,7 +141,7 @@ screen.fill(white)
 
 pygame.display.update()
 
-if algo_input == "RB" or "rb":
+if algo_input.lower() == "rb":
     maze1 = generate_rb()
 if algo_input == "prim":
     maze1 = generate_prim()
